02fe23da8792ca323ede7435b25a5add4676c5f7
kimtree24 <ktj503@naver.com>
2026-01-08 15:28:19 +0900

    [FIX] user-data 명령어 수정

--- missions/W2/M6/user-data.sh
+++ missions/W2/M6/user-data.sh
@@ -2,17 +2,14 @@
 set -e
 
 # 시스템 업데이트
-yum update -y
+dnf update -y
 
 # Docker 설치
-amazon-linux-extras install docker -y
+dnf install -y docker
 
 # Docker 서비스 시작
-systemctl start docker
 systemctl enable docker
+systemctl start docker
 
 # ec2-user를 docker 그룹에 추가
 usermod -aG docker ec2-user
-
-# 재부팅
-reboot
